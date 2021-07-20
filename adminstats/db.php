25d29cec6f69a8729516eb839a6dda908fa2bb62
Bibek Shrestha <60129668+bibek2001@users.noreply.github.com>
2021-07-20 14:09:02 +0545

    admin page deleted

--- adminstats/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect("sql212.epizy.com","epiz_27617982","7RyfVgOIpth3j","epiz_27617982_blog_admin_db");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
- ?>
